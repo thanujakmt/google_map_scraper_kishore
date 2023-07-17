5acf766124eb21e59db4343e04b23307101b1a23
thanujakmt <thanujakmsdr@gmail.com>
2023-07-17 14:33:21 +0530

    thanuja did mistake

--- niche_details.py
+++ niche_details.py
@@ -1,5 +1,5 @@
 country = 'united states of america'
-niche = 'nutritionist_business'
+niche = 'nutritionist'
 category = niche
 country_code = 'usa'
 location_table = f'{country_code}_{niche}_locations'
@@ -29,5 +29,5 @@ db_credential = {"dentist":{"user":"dentist","password":"EvenD!ngo52","database"
                 "yoga_studio":{"user":"usa_yoga_db","password":"SignBox76","database":"usa_yoga_db"},
                 "massage_therapist" :{"user":"massage_therapist_business_db","password":"he@vyCamp24","database":"massage_therapist_business_db"},
                 "naturopathic_practitioner" :{"user":"naturopathic_practitioner","password":"LionC@ve74","database":"naturopathic_practitioner_business_db"},
-                "nutritionist_business" : {"user":"nutritionist_business_db","password":"wi$eFeast47","database":"nutritionist_business_db"}
+                "nutritionist" : {"user":"nutritionist_business_db","password":"wi$eFeast47","database":"nutritionist_business_db"}
                 }
